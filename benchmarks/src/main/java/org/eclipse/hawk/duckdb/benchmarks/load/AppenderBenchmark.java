27bb838e4b82a679c9855247123d82a71d260a96
Gabor Szarnyas <szarnyasg@gmail.com>
2020-11-12 11:14:55 +0100

    Add AppenderBenchmark

---
+++ benchmarks/src/main/java/org/eclipse/hawk/duckdb/benchmarks/load/AppenderBenchmark.java
@@ -0,0 +1,44 @@
+package org.eclipse.hawk.duckdb.benchmarks.load;
+
+import org.duckdb.DuckDBAppender;
+import org.duckdb.DuckDBConnection;
+import org.eclipse.hawk.duckdb.benchmarks.Benchmark;
+import org.eclipse.hawk.duckdb.benchmarks.DataGenerator;
+import org.eclipse.hawk.duckdb.benchmarks.Row;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AppenderBenchmark extends AbstractLoadBenchmark implements Benchmark {
+	public AppenderBenchmark(DataGenerator gen, int nRows) {
+		super(gen, nRows, false);
+	}
+
+	@Override
+	protected void loadData() throws SQLException {
+		DuckDBConnection conn = (DuckDBConnection) duckDB;
+		try (DuckDBAppender appender = conn.createAppender("main", "data")) {
+			for (int i = 0; i < nRows; i++) {
+				if (autocommit && i % 100 == 0) {
+					System.out.println(
+							String.format("%s: loaded %d out of %d rows", getClass().getSimpleName(), i, nRows));
+				}
+				Row row = generator.row();
+
+				appender.beginRow();
+				appender.append(row.indexName);
+				appender.append(row.keyName);
+				appender.append(row.keyValue);
+				appender.append(row.nodeId);
+				appender.endRow();
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		return String.format("AppenderBenchmark ", autocommit);
+	}
+}
